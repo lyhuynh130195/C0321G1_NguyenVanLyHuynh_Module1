8e27db0046a14cea2fd0e32479804bd39856e92e
ly-huynh <lyhuynh130195@gmail.com>
2021-04-02 08:17:28 +0700

    commit

---
+++ 7.Bien_kieu_du_lieu/thuc_hanh/bai_2.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("nhap vao width");
+inputHeight=prompt("nhap vao height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=width*height;
+document.write("area  =   "+area);
