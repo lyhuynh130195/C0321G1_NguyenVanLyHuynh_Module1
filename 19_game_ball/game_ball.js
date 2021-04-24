f39c3aea67410f5a73a91e2e7fea80c0e1e8d1fe
ly-huynh <lyhuynh130195@gmail.com>
2021-04-24 17:04:44 +0700

    commit casestudy

--- 19_game_ball/game_ball.js
+++ 19_game_ball/game_ball.js
@@ -1,4 +1,5 @@
 let canvas, ctx;
+let score=0;
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
@@ -16,14 +17,15 @@ class Ball {
         this.x += this.speedX;
     }
 
+
     moveCollision() {
-        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+        if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
             this.speedX = -this.speedX;
         }
-        if (this.y - this.radius < 0) {
+        if (this.y - this.radius <= 0) {
             this.speedY = -this.speedY;
         }
-        if (this.y + this.radius > paddle.y+paddle.height) {
+        if (this.y + this.radius > paddle.y + paddle.height) {
             document.location.reload()
         }
     }
@@ -59,6 +61,7 @@ class Paddle {
     }
 
 }
+
 let paddle = new Paddle(100, 500, 80, 15, 25);
 //addEventListener keydown,keyup >>> ball move smooth
 document.addEventListener('keydown', function (event) {
@@ -84,21 +87,45 @@ function checkBallAndPaddle() {
     }
     if (paddle.x < 0) {
         paddle.x = 0;
-    } else if (paddle.x +paddle.width > canvas.width) {
-        paddle.x = canvas.width-paddle.width;
+    } else if (paddle.x + paddle.width > canvas.width) {
+        paddle.x = canvas.width - paddle.width;
     }
-    if (ball.y + ball.radius > paddle.y-paddle.height &&
+    if (ball.y + ball.radius > paddle.y - paddle.height &&
         ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
         ball.speedY = -ball.speedY;
+        score+=1;
+    }
+    if(ball.x===canvas.width&&ball.y===paddle.y-paddle.height){
+        ball.speedX=-ball.speedX;
+        ball.speedY=-ball.speedY;
+    }
+    if(ball.x===canvas.width-canvas.width&&ball.y===paddle.y-paddle.height){
+        ball.speedX=-ball.speedX;
+        ball.speedY=-ball.speedY;
+    }
+}
+
+function disPlayScore() {
+    if (ball.y + ball.radius == paddle.y - paddle.height &&
+        ball.x >= paddle.x && ball.x <= paddle.x + paddle.width) {
+        score += 1;
+        if(score>20){
+            ball.speedX=10;
+            ball.speedY=10;
+        }
+        document.getElementById('score').innerHTML='Score :'+score;
     }
 }
 
 function upDate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball.draw()  ;
+    ball.draw();
     paddle.drawPaddle();
     ball.move();
     ball.moveCollision();
     checkBallAndPaddle();
+    disPlayScore();
 }
-setInterval('upDate()',23)
+
+
+setInterval('upDate()', 23)
